6c2a39657839ebb0d5920a2ca192b7972423e806
Heisharly <ibraheemharly@gmail.com>
2023-07-14 13:40:45 -0700

    life

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main -display output using prinf function
+*
+*Return: always return (0)
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
+
