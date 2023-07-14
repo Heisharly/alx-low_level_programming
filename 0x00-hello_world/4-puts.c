467594ea3543361da5fa4849db7e152e628e853f
Heisharly <ibraheemharly@gmail.com>
2023-07-14 05:47:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#Include <stdio.h>
+#include <stdio.h>
 /**
- * main -prints output using put
+ * main -prints output using puts function
  *
- * Return; always (0)
+ * Return: always (0)
  */
-   int main(void)
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
