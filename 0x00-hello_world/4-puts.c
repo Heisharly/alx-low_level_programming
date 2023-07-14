a957467d58bec3d14bf836aa17428149857e4ca9
Heisharly <ibraheemharly@gmail.com>
2023-07-14 03:44:29 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#Include <stdio.h>
+/**
+ * main -prints output using put
+ *
+ * Return; always (0)
+ */
+   int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+}
