8a3cef42fac252a44d2836e3491e15e8078b4d3a
Heisharly <ibraheemharly@gmail.com>
2023-07-14 18:32:14 -0700

    unique

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main -A Program that print a line to a standard error
- * Return:This returns 1
+ * Return:This return 1
  */
 int main(void)
 {
-write("and that piece of art is useful\"- Dora Korpar,2015-10-19\n", stdout);
+write("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 
