ddd38a20229ac5d5bfe453983535ef6c4be6c696
Heisharly <ibraheemharly@gmail.com>
2023-07-19 09:47:21 -0700

    read

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char putChar[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putChar[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
