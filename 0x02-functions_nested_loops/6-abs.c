acff409606094edb7c1468196f5a0a7db9a631cc
Heisharly <ibraheemharly@gmail.com>
2023-07-19 18:22:29 -0700

    6abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+ * _abs - function to find the absolute value of a number
+ * @a: function parameters
+ * Return: (-a or a)
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
+
