2db38e96c64515656a32854843a182a16da78274
Heisharly <ibraheemharly@gmail.com>
2023-08-04 18:06:34 -0700

    libmy

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for a digit
+ * @c:function  parameters
+ * Return: 1 if success and 0 on failure
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
