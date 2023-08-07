654b9aa0ddbd95531f262f946ca2ff98048fc9f0
Heisharly <ibraheemharly@gmail.com>
2023-08-07 14:29:11 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, j, add = 0;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!(atoi(argv[i])) || atoi(argv[i]) < 0)
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
 		}
-		printf("%i\n", sum);
+		add += atoi(argv[i]);
 	}
-	else
-	{
-		printf("%i\n", 0);
-	}
+	printf("%d\n", add);
 	return (0);
 }
