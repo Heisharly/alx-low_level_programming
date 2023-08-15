088acc37976e31f8827780f7db50205cd8cdce00
Heisharly <ibraheemharly@gmail.com>
2023-08-15 15:50:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age:dogs age
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
